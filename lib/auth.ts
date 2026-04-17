2c47ddaac2ffb5525a7d575a5eb36dab2a9bbf1a
5star55 <tr6845841@gmail.com>
2026-04-17 09:27:50 +0100

    changes

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,23 +1,16 @@
 import { betterAuth } from "better-auth";
-import { dash } from "@better-auth/infra";
-import Database from "better-sqlite3";
+import { createPool } from "mysql2/promise";
 
-const dashboardApiKey = process.env.BETTER_AUTH_API_KEY;
-const dashboardApiUrl = process.env.BETTER_AUTH_API_URL;
-const dashboardKvUrl = process.env.BETTER_AUTH_KV_URL;
+const dialect = createPool({
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "database",
+  timezone: "Z",
+});
 
 export const auth = betterAuth({
-  database: new Database("./sqlite.db"),
-  emailAndPassword: {
-    enabled: true,
-  },
-  plugins: dashboardApiKey
-    ? [
-        dash({
-          apiKey: dashboardApiKey,
-          apiUrl: dashboardApiUrl,
-          kvUrl: dashboardKvUrl,
-        }),
-      ]
-    : [],
+  database: { dialect, type: "mysql" },
+  baseURL: "http://localhost:3000/",
+  emailAndPassword: { enabled: true },
 });
